2ff72dbbe47d66981061ded892af5c313aba0b14
Adam <ad.schmidt7@gmail.com>
2019-01-27 16:25:49 +1030

    Menu

---
+++ Assets/Scripts/TurnThingsOnOrOff.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnThingsOnOrOff : MonoBehaviour {
+
+    public GameObject[] turnOff;
+    public GameObject[] turnOn;
+    public string inputButton;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (inputButton != "" && Input.GetButtonDown(inputButton))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            Activate();
+        }
+	}
+
+    public void Activate()
+    {
+        foreach (GameObject obj in turnOn)
+            obj.SetActive(true);
+        foreach (GameObject obj in turnOff)
+            obj.SetActive(false);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
